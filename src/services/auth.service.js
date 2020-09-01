eb26b696f9665d4a906d363700ffccf5f4f9bc98
Agustin De Luca <agusdeluca96@gmail.com>
2020-09-01 12:16:08 -0300

    Updated API url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 import { ApiService } from "./api.service";
 
 export const AuthService = {
-  auth: () => ApiService.post(`/auth`, { apiKey: "23567b218376f79d9415" })
+  auth: () => ApiService.post("auth", { apiKey: "23567b218376f79d9415" })
 };
